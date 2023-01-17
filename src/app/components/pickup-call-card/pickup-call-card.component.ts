7e628029de644010db48fcc45cb458e696bda534
Mrigesh Deshpande <mrigeshdeshpande246@gmail.com>
2023-01-17 06:22:33 +0530

    resusable component

---
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup-call-card',
+  templateUrl: './pickup-call-card.component.html',
+  styleUrls: ['./pickup-call-card.component.scss'],
+})
+
+export class PickupCallCardComponent implements OnInit {
+  showHeader:boolean=false;
+  hasFooter:Boolean=false;
+
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,13 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { PickupCallCardComponent } from 'src/app/components/pickup-call-card/pickup-call-card.component';
+import { PickupCallPageModule } from '../pickup-call/pickup-call.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    PickupCallPageModule
+
   ],
   declarations: [HomePage]
 })
